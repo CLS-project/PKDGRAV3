18a39b334e01b7aa8fda76d4fe1e3e5bb5abafe1
Joachim Stadel <stadel@physik.uzh.ch>
2007-04-16 07:53:36 +0000

    removed headers for the particle walk functions

--- walk.h
+++ walk.h
@@ -5,6 +5,5 @@
 
 int pkdGravWalk(PKD pkd,double dTime,int nReps,int bEwald,int bVeryActive,double fEwCut,
 				double *pdFlop,double *pdPartSum,double *pdCellSum);
-void pkdParticleWalk(PKD pkd,int iParticle,int nReps, ILP **pilp, int *pnPart, int *pnMaxPart, ILC **pilc, int *pnCell, int *pnMaxCell, ILPB **pilpb, int *pnPartBucket, int *pnMaxPartBucket);
 
 #endif
