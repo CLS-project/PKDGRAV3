6e70ff1eb7943adb1fd30f0a34eed580ce3e357c
Joachim Stadel <stadel@physik.uzh.ch>
2007-04-20 17:29:32 +0000

    Return to original grav code, but calculating the new crit.

--- grav.c
+++ grav.c
@@ -179,11 +179,14 @@ int pkdGravInteract(PKD pkd,KDN *pBucket,LOCR *pLoc,ILP *ilp,int nPart,ILC *ilc,
     KDN *pkdn = pBucket;
     const double onethird = 1.0/3.0;
     double ax,ay,az,fPot;
-    double x,y,z,d2,dir,dir2;
+    double x,y,z,d2,dir,dir2,g2,g3,g4,olddir;
+    double xx,xy,xz,yy,yz,zz;
+    double xxx,xxz,yyy,yyz,xxy,xyy,xyz;
+    double tx,ty,tz;
     double fourh2;
     double rhocadd,rhocaddlocal,rholoc,rhopmax,rhopmaxlocal,costheta;
     double vx,vy,vz,v2,mu,Etot,L2,ecc,eccfac;
-    momFloat tPot,tax,tay,taz,magai,adotai;
+    momFloat tax,tay,taz,magai,adotai;
     double rhosum,maisum;
 #ifdef SOFTSQUARE
     double ptwoh2;
@@ -243,19 +246,61 @@ int pkdGravInteract(PKD pkd,KDN *pBucket,LOCR *pLoc,ILP *ilp,int nPart,ILC *ilc,
 	    z = p[i].r[2] - ilc[j].z;
 	    d2 = x*x + y*y + z*z;
 	    SQRT1(d2,dir);
-	    momEvalMomrInline(&ilc[j].mom,dir,x,y,z,&tPot,&tax,&tay,&taz,&magai);
+	    olddir = dir;
+	    dir2 = dir*dir;
+	    g2 = 3*dir*dir2*dir2;
+	    g3 = 5*g2*dir2;
+	    g4 = 7*g3*dir2;
+	    /*
+	    ** Calculate the funky distance terms.
+	    */
+	    xx = 0.5*x*x;
+	    xy = x*y;
+	    xz = x*z;
+	    yy = 0.5*y*y;
+	    yz = y*z;
+	    zz = 0.5*z*z;
+	    xxx = x*(onethird*xx - zz);
+	    xxz = z*(xx - onethird*zz);
+	    yyy = y*(onethird*yy - zz);
+	    yyz = z*(yy - onethird*zz);
+	    xx -= zz;
+	    yy -= zz;
+	    xxy = y*xx;
+	    xyy = x*yy;
+	    xyz = xy*z;
+	    /*
+	    ** Now calculate the interaction up to Hexadecapole order.
+	    */
+	    tx = g4*(ilc[j].mom.xxxx*xxx + ilc[j].mom.xyyy*yyy + ilc[j].mom.xxxy*xxy + ilc[j].mom.xxxz*xxz + ilc[j].mom.xxyy*xyy + ilc[j].mom.xxyz*xyz + ilc[j].mom.xyyz*yyz);
+	    ty = g4*(ilc[j].mom.xyyy*xyy + ilc[j].mom.xxxy*xxx + ilc[j].mom.yyyy*yyy + ilc[j].mom.yyyz*yyz + ilc[j].mom.xxyy*xxy + ilc[j].mom.xxyz*xxz + ilc[j].mom.xyyz*xyz);
+	    tz = g4*(-ilc[j].mom.xxxx*xxz - (ilc[j].mom.xyyy + ilc[j].mom.xxxy)*xyz - ilc[j].mom.yyyy*yyz + ilc[j].mom.xxxz*xxx + ilc[j].mom.yyyz*yyy - ilc[j].mom.xxyy*(xxz + yyz) + ilc[j].mom.xxyz*xxy + ilc[j].mom.xyyz*xyy);
+	    g4 = 0.25*(tx*x + ty*y + tz*z);
+	    xxx = g3*(ilc[j].mom.xxx*xx + ilc[j].mom.xyy*yy + ilc[j].mom.xxy*xy + ilc[j].mom.xxz*xz + ilc[j].mom.xyz*yz);
+	    xxy = g3*(ilc[j].mom.xyy*xy + ilc[j].mom.xxy*xx + ilc[j].mom.yyy*yy + ilc[j].mom.yyz*yz + ilc[j].mom.xyz*xz);
+	    xxz = g3*(-(ilc[j].mom.xxx + ilc[j].mom.xyy)*xz - (ilc[j].mom.xxy + ilc[j].mom.yyy)*yz + ilc[j].mom.xxz*xx + ilc[j].mom.yyz*yy + ilc[j].mom.xyz*xy);
+	    g3 = onethird*(xxx*x + xxy*y + xxz*z);
+	    xx = g2*(ilc[j].mom.xx*x + ilc[j].mom.xy*y + ilc[j].mom.xz*z);
+	    xy = g2*(ilc[j].mom.yy*y + ilc[j].mom.xy*x + ilc[j].mom.yz*z);
+	    xz = g2*(-(ilc[j].mom.xx + ilc[j].mom.yy)*z + ilc[j].mom.xz*x + ilc[j].mom.yz*y);
+	    g2 = 0.5*(xx*x + xy*y + xz*z);
+	    dir *= ilc[j].mom.m;
+	    dir2 *= dir + 5*g2 + 7*g3 + 9*g4;
+	    fPot -= dir + g2 + g3 + g4;
+	    tax = xx + xxx + tx - x*dir2;
+	    tay = xy + xxy + ty - y*dir2;
+	    taz = xz + xxz + tz - z*dir2;
 	    rhoenc[j].index = j;
 	    rhoenc[j].x = x;
 	    rhoenc[j].y = y;
 	    rhoenc[j].z = z;
 	    rhoenc[j].dir = dir;
-	    rhoenc[j].rhoenc = magai*dir;
+	    rhoenc[j].rhoenc = dir2;
 	    adotai = p[i].a[0]*tax + p[i].a[1]*tay + p[i].a[2]*taz; 
 	    if (adotai > 0) {
-	      rhosum += adotai*dir;
-	      maisum += magai;
+	      rhosum += adotai*olddir;
+	      maisum += sqrt(tax*tax + tay*tay + taz*taz);
 	    }
-	    fPot += tPot;
 	    ax += tax;
 	    ay += tay;
 	    az += taz;
@@ -416,11 +461,13 @@ int pkdGravInteract(PKD pkd,KDN *pBucket,LOCR *pLoc,ILP *ilp,int nPart,ILC *ilc,
 	    tax = -x*dir2;
 	    tay = -y*dir2;
 	    taz = -z*dir2;
+#if 0
 	    adotai = p[i].a[0]*tax + p[i].a[1]*tay + p[i].a[2]*taz;
 	    if (adotai > 0) {
 	      rhosum += adotai*dir;
 	      maisum += ilp[j].m*magai;
 	    }
+#endif
 	    fPot -= ilp[j].m*dir;
 	    ax += tax;
 	    ay += tay;
