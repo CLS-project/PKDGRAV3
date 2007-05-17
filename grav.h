9441f9de5ea4e61055c0d24c3f718dab69c7719f
Doug Potter <dpotter@physik.uzh.ch>
2007-05-17 20:52:42 +0000

    Added PPInteractSIMD

--- grav.h
+++ grav.h
@@ -4,6 +4,10 @@
 #include "pkd.h"
 #include "moments.h"
 
+void PPInteractSIMD( int nPart, ILP *ilp, const FLOAT *r, const FLOAT *a,
+		     FLOAT fMass, FLOAT fSoft,
+		     float *ax, float *ay, float *az, float *fPot );
+
 int pkdGravInteract(PKD pkd,KDN *pBucket,LOCR *l,ILP *ilp,int nPart,ILC *ilc,int nCell,ILPB *ilpb,int nPartBucket,double *pdFlop);
 
 #endif
