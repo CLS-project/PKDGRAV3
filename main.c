ac81748b060a1762a79fc0822c252befd7d087f4
Doug Potter <dpotter@physik.uzh.ch>
2007-10-23 06:22:27 +0000

    The .log file is now opened for append instead of for write.

--- main.c
+++ main.c
@@ -151,7 +151,7 @@ int main(int argc,char **argv) {
     */
     if (msrLogInterval(msr)) {
 	sprintf(achFile,"%s.log",msrOutName(msr));
-	fpLog = fopen(achFile,"w");
+	fpLog = fopen(achFile,"a");
 	assert(fpLog != NULL);
 	setbuf(fpLog,(char *) NULL); /* no buffering */
 	/*
