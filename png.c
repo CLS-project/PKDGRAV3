1e00882a5eeaa10d7c4643926c4ed1cea8ed735d
Doug Potter <dpotter@physik.uzh.ch>
2007-11-14 14:54:33 +0000

    Changed the wrbb color map to use a function rather than 125 discrete values. There are now effectively 1280 unique colors.

--- png.c
+++ png.c
@@ -8,63 +8,36 @@
 
 #include "png.h"
 
-static void wrbb( int *cmap, gdImagePtr im ) {
-    double slope, offset;
-    int i;
+/* Famous white, red, blue, black colour map */
+static int wrbb( float v )
+{
+    if ( v < 0.0 ) v = 0.0;
+    else if ( v > 1.0 ) v = 1.0;
 
-    // Shamefully stolen from Tipsy
+    v *= 6;
 
-    slope = 255./20. ;
-    for(i = 0 ;i < 21 ;i++){
-	cmap[i] = gdImageColorAllocate(im,0,0,(int)(slope * (double)(i) + .5)) ;
-    }
-    slope = 191./20. ;
-    offset = 64. - slope * 21. ;
-    for(i = 21 ;i < 42 ;i++){
-	cmap[i] = gdImageColorAllocate(im,(int)(slope*(double)(i) + offset + .5),0,255);
-    }
-    slope = -205./21. ;
-    offset = 255. - slope * 41. ;
-    for(i = 42 ;i < 63 ;i++){
-	cmap[i] = gdImageColorAllocate(im, 255,0,(int)(slope*(double)(i) + offset + .5));
-    }
-    slope = 205./40. ;
-    offset = 50. - slope * 63. ;
-    for(i = 63 ;i < 104 ;i++){
-	cmap[i] = gdImageColorAllocate(im,255,(int)(slope*(double)(i) + offset + .5),0);
-    }
-    slope = 255./21. ;
-    offset = -slope * 103. ;
-    for(i = 104 ;i < 125 ;i++){
-	cmap[i] = gdImageColorAllocate(im,255,255,(int)(slope*(double)(i) + offset +.5));
-    }
-
-    // The MARK color
-    cmap[125] = gdImageColorAllocate(im,255,255,255);
-    cmap[126] = gdBrushed;
+    if      ( v < 1 ) return gdTrueColor(0,0,(int)(255*v));
+    else if ( v < 2 ) return gdTrueColor((int)(255*(v-1.0)),0,255);
+    else if ( v < 3 ) return gdTrueColor(255,0,(int)(255*(3.0-v)));
+    else if ( v < 5 ) return gdTrueColor(255,(int)(255*(v-3.0)*0.5),0);
+    else if ( v < 6 ) return gdTrueColor(255,255,(int)(255*(v-5.0)));
+    else              return gdTrueColor(255,255,255);
 }
-
-static const int BWIDTH = 2; //!< Width of the brush
 
 void pngWrite( PNG png, FILE *fp, float *Density )
 {
-    float v, color_slope;
+    float v, color_scale;
     uint_fast32_t i;
     int x, y;
 
-    color_slope = 124.0 / (png->maxValue - png->minValue);
+    color_scale = 1.0 / (png->maxValue - png->minValue);
     for( x=0; x<png->iResolution; x++ ) {
 	for( y=0; y<png->iResolution; y++ ) {
-	    int clr;
 	    v = Density[x+png->iResolution*y];
 	    if ( v < 1e-10 ) v = -10;
 	    else v = log10f(v);
-	    v = color_slope * ( v - png->minValue ) + 0.5;
-	    clr = (int)(v);
-	    if ( clr < 0 ) clr = 0;
-	    if ( clr > 124 ) clr = 124;
-	    assert( clr >= 0 && clr < 125 );
-	    gdImageSetPixel(png->ic,x,y,png->cmap[clr]);
+	    v = color_scale * ( v - png->minValue );
+	    gdImageSetPixel(png->ic,x,y,wrbb(v));
 	}
     }
     gdImagePng( png->ic, fp );
@@ -80,12 +53,7 @@ PNG pngInitialize( int iResolution, float minValue, float maxValue )
     png->iResolution = iResolution;
     png->minValue = minValue;
     png->maxValue = maxValue;
-
-    png->brush = gdImageCreate(BWIDTH,BWIDTH);
-    gdImageFilledRectangle(png->brush,0,0,BWIDTH-1,BWIDTH-1,
-			   gdImageColorAllocate(png->brush,0,255,0));
-    png->ic = gdImageCreate(png->iResolution,png->iResolution);
-    wrbb(png->cmap,png->ic);
+    png->ic = gdImageCreateTrueColor(png->iResolution,png->iResolution);
 
     return png;
 }
@@ -93,6 +61,5 @@ PNG pngInitialize( int iResolution, float minValue, float maxValue )
 void pngFinish( PNG png )
 {
     gdImageDestroy(png->ic);
-    gdImageDestroy(png->brush);
     free(png);
 }
--- png.h
+++ png.h
@@ -6,11 +6,7 @@ typedef struct png
     int iResolution;
     float minValue;
     float maxValue;
-
     gdImagePtr ic;
-    gdImagePtr brush;
-
-    int cmap[128];
 } *PNG;
 
 PNG pngInitialize( int iResolution, float minValue, float maxValue );
