54b6108f968cd2a8d1bde74bd2f75dd17a3aaa3a
Joachim Stadel <stadel@physik.uzh.ch>
2008-04-11 04:02:59 +0000

    p->iRung is now called p->uRung.

--- outtype.c
+++ outtype.c
@@ -28,7 +28,7 @@ FLOAT ArrType(PKD pkd,PARTICLE *p,int iType) {
     case OUT_AMAG_ARRAY:
 	return(sqrt(p->a[0]*p->a[0] + p->a[1]*p->a[1] + p->a[2]*p->a[2]));
     case OUT_RUNG_ARRAY:
-	return(p->iRung);
+	return(p->uRung);
     case OUT_DT_ARRAY:
 	return(p->dt);
     case OUT_SOFT_ARRAY:
