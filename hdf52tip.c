0d280fa9ebfcb081ad52aa8fe8c2fae092f8ca92
Doug Potter <dpotter@physik.uzh.ch>
2008-06-09 10:20:49 +0000

    Added support for native binary format.

--- hdf52tip.c
+++ hdf52tip.c
@@ -12,15 +12,18 @@
 #define BUFFER_SIZE (8*1024*1024)
 
 #define OPT_DOUBLE 'd'
+#define OPT_NATIVE 'n'
 
 int main( int argc, char *argv[] ) {
     int bError = 0;
     int bDouble = 0;
+    int bNative = 0;
     const char *inName  = 0;
     const char *outName = 0;
 
     hid_t fileID;
     IOHDF5 io;
+    IOHDF5V ioPot;
     FILE  *fout;
     char  *fout_buffer = 0;
     XDR xdr;
@@ -39,17 +42,21 @@ int main( int argc, char *argv[] ) {
 	int c, option_index=0;
 
 	static struct option long_options[] = {
-		{ "double",       0, 0, OPT_DOUBLE
-		}, { NULL,   0, 0, 0 },
+		{ "double",       0, 0, OPT_DOUBLE},
+		{ "native",       0, 0, OPT_NATIVE},
+		{ NULL,   0, 0, 0 },
 	    };
 
-	c = getopt_long( argc, argv, "d",
+	c = getopt_long( argc, argv, "dn",
 			 long_options, &option_index );
 	if ( c == -1 ) break;
 
 	switch (c) {
 	case OPT_DOUBLE:
 	    bDouble = 1;
+	    break;
+	case OPT_NATIVE:
+	    bNative = 1;
 	    break;
 	default:
 	    bError = 1;
@@ -86,6 +93,8 @@ int main( int argc, char *argv[] ) {
 	}
     io = ioHDF5Initialize( fileID, 32768, IOHDF5_SINGLE );
 
+    ioPot = ioHDFF5OpenVector( io, "potential",IOHDF5_SINGLE );
+
     fout = fopen( outName, "wb" );
     if ( fout == NULL ) {
 	fprintf( stderr, "Unable to open %s for writing\n", outName );
@@ -94,7 +103,7 @@ int main( int argc, char *argv[] ) {
     fout_buffer = malloc(BUFFER_SIZE);
     setvbuf(fout,fout_buffer,_IOFBF,BUFFER_SIZE);
 
-    xdrstdio_create(&xdr,fout,XDR_ENCODE);
+    if ( !bNative ) xdrstdio_create(&xdr,fout,XDR_ENCODE);
 
     assert(ioHDF5ReadAttribute( io, "dTime", H5T_NATIVE_DOUBLE, &dTime ));
     assert(ioHDF5GasCount(io)+ioHDF5DarkCount(io)+ioHDF5StarCount(io)<=UINT_MAX);
@@ -110,19 +119,30 @@ int main( int argc, char *argv[] ) {
 	    nDark, nGas, nStar, nBodies );
 
     /* Tipsy Header */
-    assert( xdr_double(&xdr,&dTime) );
-    assert( xdr_u_int(&xdr,&nBodies) );
-    assert( xdr_u_int(&xdr,&nDims) );
-    assert( xdr_u_int(&xdr,&nGas) );
-    assert( xdr_u_int(&xdr,&nDark) );
-    assert( xdr_u_int(&xdr,&nStar) );
-    assert( xdr_u_int(&xdr,&nPad) );
-
+    if ( bNative ) {
+	fwrite(&dTime,  sizeof(dTime),   1, fout );
+	fwrite(&nBodies,sizeof(nBodies), 1, fout );
+	fwrite(&nDims,  sizeof(nDims),   1, fout );
+	fwrite(&nGas,   sizeof(nGas),    1, fout );
+	fwrite(&nDark,  sizeof(nDark),   1, fout );
+	fwrite(&nStar,  sizeof(nStar),   1, fout );
+	fwrite(&nPad,   sizeof(nPad),    1, fout );
+	}
+    else {
+	assert( xdr_double(&xdr,&dTime) );
+	assert( xdr_u_int(&xdr,&nBodies) );
+	assert( xdr_u_int(&xdr,&nDims) );
+	assert( xdr_u_int(&xdr,&nGas) );
+	assert( xdr_u_int(&xdr,&nDark) );
+	assert( xdr_u_int(&xdr,&nStar) );
+	assert( xdr_u_int(&xdr,&nPad) );
+	}
     /* Okay, we should really do this */
     assert( nGas == 0 );
     assert( nStar == 0 );
 
     while ( ioHDF5GetDark( io, &iOrder, r, v, &fMass, &fSoft, &fPot ) ) {
+	fPot = ioHDF5GetVector(ioPot);
 	if ( fMass == 0.0 ) {
 	    fprintf(stderr, "%ld: zero mass\n", iOrder );
 	    }
@@ -133,25 +153,45 @@ int main( int argc, char *argv[] ) {
 	    fprintf(stderr, "%ld: zero velocity\n", iOrder );
 	    }
 
-	fTemp = fMass; xdr_float(&xdr,&fTemp);
-	if ( bDouble ) {
-	    xdr_double(&xdr,&r[0]);
-	    xdr_double(&xdr,&r[1]);
-	    xdr_double(&xdr,&r[2]);
+	if ( bNative ) {
+	    fTemp = fMass; fwrite(&fTemp, sizeof(fTemp), 1, fout);
+	    if ( bDouble ) {
+		fwrite( &r[0], sizeof(r[0]), 1, fout );
+		fwrite( &r[1], sizeof(r[1]), 1, fout );
+		fwrite( &r[2], sizeof(r[2]), 1, fout );
+		}
+	    else {
+		fTemp = r[0];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+		fTemp = r[1];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+		fTemp = r[2];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+		}
+	    fTemp = v[0];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+	    fTemp = v[1];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+	    fTemp = v[2];  fwrite( &fTemp, sizeof(fTemp), 1, fout );
+	    fTemp = fSoft; fwrite( &fTemp, sizeof(fTemp), 1, fout );
+	    fTemp = fPot;  fwrite( &fTemp, sizeof(fTemp), 1, fout );
 	    }
 	else {
-	    fTemp = r[0];  xdr_float(&xdr,&fTemp);
-	    fTemp = r[1];  xdr_float(&xdr,&fTemp);
-	    fTemp = r[2];  xdr_float(&xdr,&fTemp);
+	    fTemp = fMass; xdr_float(&xdr,&fTemp);
+	    if ( bDouble ) {
+		xdr_double(&xdr,&r[0]);
+		xdr_double(&xdr,&r[1]);
+		xdr_double(&xdr,&r[2]);
+		}
+	    else {
+		fTemp = r[0];  xdr_float(&xdr,&fTemp);
+		fTemp = r[1];  xdr_float(&xdr,&fTemp);
+		fTemp = r[2];  xdr_float(&xdr,&fTemp);
+		}
+	    fTemp = v[0];  xdr_float(&xdr,&fTemp);
+	    fTemp = v[1];  xdr_float(&xdr,&fTemp);
+	    fTemp = v[2];  xdr_float(&xdr,&fTemp);
+	    fTemp = fSoft; xdr_float(&xdr,&fTemp);
+	    fTemp = fPot;  xdr_float(&xdr,&fTemp);
 	    }
-	fTemp = v[0];  xdr_float(&xdr,&fTemp);
-	fTemp = v[1];  xdr_float(&xdr,&fTemp);
-	fTemp = v[2];  xdr_float(&xdr,&fTemp);
-	fTemp = fSoft; xdr_float(&xdr,&fTemp);
-	fTemp = fPot;  xdr_float(&xdr,&fTemp);
 	}
 
-    xdr_destroy(&xdr);
+    if ( !bNative ) xdr_destroy(&xdr);
     fclose(fout);
     free(fout_buffer);
     H5Fclose(fileID);
