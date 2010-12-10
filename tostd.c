7a65b04295a5d728eae8e3e0c724f4af86afdbfd
Doug Potter <dpotter@physik.uzh.ch>
2010-12-10 13:56:12 +0100

    Added support for writing density (HDF5 format only).

--- tostd.c
+++ tostd.c
@@ -13,13 +13,15 @@
 #define OPT_NATIVE 'n'
 #define OPT_HDF5   '5'
 #define OPT_POTENTIAL 'p'
+#define OPT_DENSITY   'r'
 
-int main( int argc, char **argv ) {
+int main( int argc, char *argv[] ) {
     int bError = 0;
     int bDouble = 0;
     int bNative = 0;
     int bHDF5 = 0;
     int bPotential = 0;
+    int bDensity = 0;
     uint64_t N, nSph, nDark, nStar, i;
     double dTime;
 
@@ -42,10 +44,11 @@ int main( int argc, char **argv ) {
 		{ "native",       0, 0, OPT_NATIVE},
 		{ "hdf5",         0, 0, OPT_HDF5},
 		{ "potential",    0, 0, OPT_POTENTIAL},
+		{ "density"  ,    0, 0, OPT_DENSITY},
 		{ NULL,   0, 0, 0 },
 	    };
 
-	c = getopt_long( argc, argv, "dn5p",
+	c = getopt_long( argc, argv, "dn5pr",
 			 long_options, &option_index );
 	if ( c == -1 ) break;
 
@@ -61,6 +64,9 @@ int main( int argc, char **argv ) {
 	    break;
 	case OPT_POTENTIAL:
 	    bPotential = 1;
+	    break;
+	case OPT_DENSITY:
+	    bDensity = 1;
 	    break;
 	default:
 	    bError = 1;
@@ -114,6 +120,7 @@ int main( int argc, char **argv ) {
 	int mFlag = FIO_FLAG_COMPRESS_MASS | FIO_FLAG_COMPRESS_SOFT;
 	if (bDouble) mFlag |= FIO_FLAG_DOUBLE_POS | FIO_FLAG_DOUBLE_VEL;
 	if (bPotential) mFlag |= FIO_FLAG_POTENTIAL;
+	if (bDensity) mFlag |= FIO_FLAG_DENSITY;
 	fioOut = fioHDF5Create(outName,mFlag);
 	}
     else {
