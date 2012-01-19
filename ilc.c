e1c88d9c1ab4c4888a4b9a4435fb92cea9498c88
Doug Potter <dpotter@physik.uzh.ch>
2012-01-19 15:49:04 +0100

    The ILC now needs to allocate memory via CUDA if CUDA is enabled, otherwise the stream is not asynchronous.

--- ilc.c
+++ ilc.c
@@ -5,12 +5,17 @@
 #include <assert.h>
 
 #include "ilc.h"
+#include "cudautil.h"
 
 void ilcInitialize(ILC *ilc) {
     *ilc = malloc(sizeof(struct ilcContext));
     assert( *ilc != NULL );
     lstInitialize(&(*ilc)->lst,NULL,ILC_BLK_PER_TILE, ILC_PART_PER_BLK, 2, /* two areas */
+#ifdef USE_CUDA
+	sizeof(ILC_BLK),CUDA_malloc,CUDA_free,
+#else
 	sizeof(ILC_BLK),SIMD_malloc,SIMD_free,
+#endif
 	sizeof(ILC_XTR),NULL,           NULL);
     (*ilc)->cx = (*ilc)->cy = (*ilc)->cz = 0.0;
     }
