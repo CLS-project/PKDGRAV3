55b28d329d9eee4fc120547aef6b22aa339b6321
Joachim Stadel <stadel@physik.uzh.ch>
2016-01-28 00:07:21 +0100

    Added fof.h

---
+++ fof.h
@@ -0,0 +1,11 @@
+#ifndef FOF_INCLUDED
+#define FOF_INCLUDED
+
+#include "pkd.h"
+
+int pkdNewFof(PKD pkd,double dTau2,int nMinMembers);
+int pkdFofPhases(PKD pkd);
+uint64_t pkdFofFinishUp(PKD pkd,int nMinGroupSize);
+
+#endif
+
