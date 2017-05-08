7d88b67da9e0e926809b4a2156d89d20b971daf7
Doug Potter <DougPotter@DougPotter.com>
2017-05-08 18:43:04 +0200

    Now includes a version of openpa.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@
 # touch NEWS README AUTHORS ChangeLog
 # This should be done when Makefile.am or configure.ac change.  Best to do
 # a "make distclean" if already configured before running this script.
+test -d openpa || tar -xf openpa.tar.bz2
 ( cd openpa ; autoheader ; aclocal -Iconfdb ; automake -acf ; autoconf -i -f )
 autoheader
 aclocal -Im4 -I$(gsl-config --prefix)/share/aclocal
