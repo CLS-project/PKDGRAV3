9cf84eb81f33f64d03bbf496170efadbcb803226
Doug Potter <DougPotter@DougPotter.com>
2019-07-02 16:44:46 +0200

    Correct the variable used (gave compilation error)

--- measurepk.cxx
+++ measurepk.cxx
@@ -144,7 +144,7 @@ void pkdMeasurePk(PKD pkd, double dTotalMass, int iAssignment, int bInterlace,
 	    fK[ks] += log(ak);
 	    fPower[ks] += std::norm(v1);
 	    nPower[ks] += 1;
-	    if (ix!=0 && ix!=iNyquist) { // Account for negative Kx values
+	    if (i!=0 && i!=iNyquist) { // Account for negative Kx values
 		fK[ks] += log(ak);
 		fPower[ks] += std::norm(v1);
 		nPower[ks] += 1;
