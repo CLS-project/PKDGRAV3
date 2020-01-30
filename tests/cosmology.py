41b41d7cf6d15bbca4e77ce6791be75265c177d7
Doug Potter <DougPotter@DougPotter.com>
2020-01-30 20:32:27 +0100

    Include test modules in build image and make float tests more forgiving

--- tests/cosmology.py
+++ tests/cosmology.py
@@ -12,14 +12,14 @@ class TestCosmology(unittest.TestCase):
         self.csm.SetCosmology(dHubble0=math.sqrt(math.pi*8/3),dOmega0=0.32,dSigma8=0.83,ns=0.96)
 
     def testHubble(self):
-        self.assertEqual(self.csm.Exp2Hub(1.0), math.sqrt(math.pi*8/3))
-        self.assertEqual(self.csm.Exp2Hub(0.1),57.013166890765135)
-        self.assertEqual(self.csm.Exp2Hub(0.01),1654.627836659466)
+        self.assertAlmostEqual(self.csm.Exp2Hub(1.0), math.sqrt(math.pi*8/3),places=15)
+        self.assertAlmostEqual(self.csm.Exp2Hub(0.1),57.013166890765135,places=15)
+        self.assertAlmostEqual(self.csm.Exp2Hub(0.01),1654.627836659466,places=15)
 
     def testExp2Time(self):
-        self.assertEqual(self.csm.Exp2Time(1.0),0.27718310726907297)
-        self.assertEqual(self.csm.Exp2Time(0.1),0.012137233682214097)
-        self.assertEqual(self.csm.Exp2Time(0.01),0.0004046022074540854)
+        self.assertAlmostEqual(self.csm.Exp2Time(1.0),0.27718310726907297,places=15)
+        self.assertAlmostEqual(self.csm.Exp2Time(0.1),0.012137233682214097,places=15)
+        self.assertAlmostEqual(self.csm.Exp2Time(0.01),0.0004046022074540854,places=15)
 
     def testTime2Exp(self):
         self.assertAlmostEqual(self.csm.Time2Exp(0.27718310726907297),1.0,places=15)
