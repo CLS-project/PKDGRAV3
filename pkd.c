5ebea5901379e80bd2aa77570da231d5b5595095
Doug Potter <DougPotter@DougPotter.com>
2020-02-07 11:19:31 +0100

    FIX: correct use of predicate when it is not zero or one.

--- pkd.c
+++ pkd.c
@@ -3220,8 +3220,8 @@ double pkdTotalMass(PKD pkd) {
 ** Conflicting options (e.g., setIfTrue and setIfFalse) result in a toggle.
 */
 static inline int isSelected( int predicate, int setIfTrue, int clearIfFalse, int value ) {
-    int s = (predicate&(setIfTrue>0)) | (~predicate&(clearIfFalse<0));
-    int c = (predicate&(setIfTrue<0)) | (~predicate&(clearIfFalse>0));
+    int s = ((predicate!=0)&(setIfTrue>0)) | (~(predicate!=0)&(clearIfFalse<0));
+    int c = ((predicate!=0)&(setIfTrue<0)) | (~(predicate!=0)&(clearIfFalse>0));
     return (~s&~c&value) | (s&~(c&value));
     }
 
