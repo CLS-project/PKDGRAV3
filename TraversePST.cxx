3c8be1829c0314da2d45e7702104a3699cf454d8
Doug Potter <DougPotter@DougPotter.com>
2021-08-04 17:31:36 +0200

    Add missing TraversePST class.

---
+++ TraversePST.cxx
@@ -0,0 +1,39 @@
+/*  This file is part of PKDGRAV3 (http://www.pkdgrav.org/).
+ *  Copyright (c) 2001-2018 Joachim Stadel & Douglas Potter
+ *
+ *  PKDGRAV3 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PKDGRAV3 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PKDGRAV3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "TraversePST.h"
+
+int TraversePST::operator()(int nIn, void *pIn, void *pOut) {
+    return Traverse(node_pst,pIn,nIn,pOut,getMaxBytesOut());
+    }
+
+int TraversePST::Traverse(PST pst,void *vin,int nIn,void *vout,int nOut) {
+    if (pstAmCore(pst))       return Service(pst,vin,nIn,vout,nOut);
+    else if (pstOffNode(pst)) return OffNode(pst,vin,nIn,vout,nOut);
+    else if (pstAmNode(pst))  return  AtNode(pst,vin,nIn,vout,nOut);
+    else                      return Recurse(pst,vin,nIn,vout,nOut);
+    }
+
+// This is the default function for when we are still traversing the PST
+// (OffNode,AtNode,Recurse) and will just naively recurse. Once we reach
+// the leaf (AmCore) then Service() is called and must be provided.
+int TraversePST::Recurse(PST pst,void *vin,int nIn,void *vout,int nOut) {
+    auto mdl = reinterpret_cast<mdl::mdlClass *>(pst->mdl);
+    auto rID = mdl->ReqService(pst->idUpper,getServiceID(),vin,nIn);
+    Traverse(pst->pstLower,vin,nIn,vout,nOut);
+    mdl->GetReply(rID,vout,&nOut);
+    return nOut;
+    }
---
+++ TraversePST.h
@@ -0,0 +1,39 @@
+/*  This file is part of PKDGRAV3 (http://www.pkdgrav.org/).
+ *  Copyright (c) 2001-2018 Joachim Stadel & Douglas Potter
+ *
+ *  PKDGRAV3 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PKDGRAV3 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PKDGRAV3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef TRAVERSEPST_H
+#define TRAVERSEPST_H
+#include "pst.h"
+#include <string>
+
+class TraversePST : public mdl::BasicService {
+    PST node_pst;
+public:
+    explicit TraversePST(PST node_pst,int service_id,
+    	int nInBytes=0, int nOutBytes=0, const char *service_name=nullptr)
+	: BasicService(service_id, nInBytes, nOutBytes, service_name), node_pst(node_pst) {}
+    virtual ~TraversePST() = default;
+protected:
+    virtual int operator()(int nIn, void *pIn, void *pOut) override;
+    virtual int Traverse(PST pst,void *vin,int nIn,void *vout,int nOut);
+
+    virtual int OffNode(PST pst,void *vin,int nIn,void *vout,int nOut) {return Recurse(pst,vin,nIn,vout,nOut);}
+    virtual int  AtNode(PST pst,void *vin,int nIn,void *vout,int nOut) {return Recurse(pst,vin,nIn,vout,nOut);}
+    virtual int Recurse(PST pst,void *vin,int nIn,void *vout,int nOut);
+    virtual int Service(PST pst,void *vin,int nIn,void *vout,int nOut) = 0;
+    };
+
+#endif
