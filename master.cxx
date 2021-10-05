6feae0d047718cdf2bd5ec214dd933e901fcfd18
Douglas Potter <douglas.potter@uzh.ch>
2021-10-05 16:50:02 +0200

    fixed struct warning message

--- master.cxx
+++ master.cxx
@@ -1945,7 +1945,7 @@ void MSR::BuildTree(int bNeedEwald,uint32_t uRoot,uint32_t utRoot) {
     auto nMsgSize = sizeof(ServiceDistribTopTree::input) + nTopTree;
 
     std::unique_ptr<char[]> buffer {new char[nMsgSize]};
-    auto pDistribTop = new (buffer.get()) struct ServiceDistribTopTree::input;
+    auto pDistribTop = new (buffer.get()) ServiceDistribTopTree::input;
     auto pkdn = reinterpret_cast<KDN*>(pDistribTop + 1);
     pDistribTop->uRoot = uRoot;
 
