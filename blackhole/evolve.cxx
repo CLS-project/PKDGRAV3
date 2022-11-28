CLS-project/PKDGRAV3

commit 1f8e8f88b143c02884d9d6d86e185bb0a60d9cc4

PKDGRAV-149 Treat AGN feedback the same as SN feedback

blackhole/evolve.cxx
@@ -65,15 +65,20 @@ static inline void bhFeedback(PKD pkd, NN *nnList, int nSmooth, int naccreted, P
                 SPHFIELDS *qsph = pkdSph(pkd,q);
                 if (qsph->BHAccretor.iPid != NOT_ACCRETED) continue; // Skip accreted particles
                 printf("BH feedback event!\n");
-                const double energy = dBHFBEcrit * pkdMass(pkd,q) ;
-                qsph->Uint += energy;
-                qsph->E += energy;
+                const double dEnergyInput = dBHFBEcrit * pkdMass(pkd,q) ;
+
+#ifdef OLD_FB_SCHEME
+                qsph->Uint += dEnergyInput;
+                qsph->E += dEnergyInput;
 #ifdef ENTROPY_SWITCH
-                qsph->S += energy*(dConstGamma-1.) *
+                qsph->S += dEnergyInput*(dConstGamma-1.) *
                            pow(pkdDensity(pkd,q), -dConstGamma+1);
 #endif
+#else // OLD_FB_SCHEME
+                qsph->fAccFBEnergy += dEnergyInput;
+#endif
 
-                pBH->dAccEnergy -= energy;
+                pBH->dAccEnergy -= dEnergyInput;
             }
         }
     }

pkd.c
@@ -1097,7 +1097,7 @@ void pkdReadFIO(PKD pkd,FIO fio,uint64_t iFirst,int nLocal,double dvFac, double 
                     pSph->lastCooling = 0.;
                     pSph->cooling_dudt = 0.;
 #endif
-#ifdef FEEDBACK
+#if defined(FEEDBACK) || defined(BLACKHOLES)
                     pSph->fAccFBEnergy = 0.;
 #endif
 #ifdef BLACKHOLES
@@ -2890,7 +2890,7 @@ void pkdEndTimestepIntegration(PKD pkd, struct inEndTimestep in) {
             pkdGrackleCooling(pkd, p, pDelta, in.dTuFac);
 #endif
 
-#ifdef FEEDBACK
+#if defined(FEEDBACK) || defined(BLACKHOLES)
             // ##### Apply feedback
             pkdAddFBEnergy(pkd, p, psph, in.dConstGamma);
 #endif

pkd.h
@@ -274,7 +274,7 @@ typedef struct sphfields {
 #endif
 
 
-#ifdef FEEDBACK
+#if defined(FEEDBACK) || defined(BLACKHOLES)
     float fAccFBEnergy;
 #endif
 
@@ -1101,6 +1101,20 @@ static inline void pkdNodeSetBndMinMax( PKD pkd, KDN *n, double *dMin, double *d
     }
     pkdNodeSetBnd(pkd,n,&bnd);
 }
+
+#if defined(FEEDBACK) || defined(BLACKHOLES)
+static inline void pkdAddFBEnergy(PKD pkd, PARTICLE *p, SPHFIELDS *psph, double dConstGamma) {
+#ifndef OLD_FB_SCHEME
+    psph->Uint += psph->fAccFBEnergy;
+    psph->E += psph->fAccFBEnergy;
+#ifdef ENTROPY_SWITCH
+    psph->S += psph->fAccFBEnergy*(dConstGamma-1.) *
+               pow(pkdDensity(pkd,p), -dConstGamma+1);
+#endif
+    psph->fAccFBEnergy = 0.0;
+#endif //OLD_FB_SCHEME
+}
+#endif
 
 #ifdef OPTIM_REORDER_IN_NODES
 static inline int pkdNodeNgas( PKD pkd, KDN *n) {

starformation/feedback.h
@@ -7,18 +7,6 @@ void smSNFeedback(PARTICLE *p,float fBall,int nSmooth,NN *nnList,SMF *smf) ;
 void initSNFeedback(void *vpkd, void *vp);
 void combSNFeedback(void *vpkd, void *v1, const void *v2);
 
-static inline void pkdAddFBEnergy(PKD pkd, PARTICLE *p, SPHFIELDS *psph, double dConstGamma) {
-#ifndef OLD_FB_SCHEME
-    psph->Uint += psph->fAccFBEnergy;
-    psph->E += psph->fAccFBEnergy;
-#ifdef ENTROPY_SWITCH
-    psph->S += psph->fAccFBEnergy*(dConstGamma-1.) *
-               pow(pkdDensity(pkd,p), -dConstGamma+1);
-#endif
-    psph->fAccFBEnergy = 0.0;
-#endif //OLD_FB_SCHEME
-}
-
 static inline float SNFeedbackEfficiency(float Z, float rho, double dSNFBEfficiency,
         double dSNFBMaxEff, double dSNFBEffIndex,
         double dSNFBEffnH0) {
@@ -31,7 +19,6 @@ static inline float SNFeedbackEfficiency(float Z, float rho, double dSNFBEfficie
         return dSNFBEfficiency;
     }
 }
-
 
 #ifdef __cplusplus
 }

commit c135aba6488c360fa2b5d3a6ecb49d4f03008af3

PKDGRAV-149 Fixed a bug introduced in 1f8e8f88 which prevented the update of received BH FB energy in a remote gas particle

blackhole/evolve.cxx
@@ -315,31 +315,27 @@ void combBHevolve(void *vpkd, void *vp1,const void *vp2) {
     PARTICLE *p1 = (PARTICLE *) vp1;
     PARTICLE *p2 = (PARTICLE *) vp2;
 
-    int pSpecies2 = pkdSpecies(pkd, p2);
-
-    if (pSpecies2 == FIO_SPECIES_SPH) {
+    if (pkdIsGas(pkd,p1) && pkdIsGas(pkd,p2)) {
+        SPHFIELDS *psph1 = pkdSph(pkd,p1);
         SPHFIELDS *psph2 = pkdSph(pkd,p2);
 
-        if (psph2->Uint > 0.0) {
-            SPHFIELDS *psph1 = pkdSph(pkd,p1);
-
-            psph1->Uint += psph2->Uint;
-            psph1->E += psph2->E;
+#ifdef OLD_FB_SCHEME
+        psph1->Uint += psph2->Uint;
+        psph1->E += psph2->E;
 #ifdef ENTROPY_SWITCH
-            psph1->S += psph2->S;
+        psph1->S += psph2->S;
+#endif
+#else //OLD_FB_SCHEME
+        psph1->fAccFBEnergy += psph2->fAccFBEnergy;
 #endif
 
+        // Is this the first accretion attempt for this particle?
+        if (psph1->BHAccretor.iPid == NOT_ACCRETED &&
+                psph2->BHAccretor.iPid != NOT_ACCRETED) {
+            psph1->BHAccretor.iPid   = psph2->BHAccretor.iPid;
+            psph1->BHAccretor.iIndex = psph2->BHAccretor.iIndex;
         }
-
-        if (psph2->BHAccretor.iPid != NOT_ACCRETED) {
-            SPHFIELDS *psph1 = pkdSph(pkd,p1);
-            if (psph1->BHAccretor.iPid == NOT_ACCRETED) {
-                // First try to accrete this particle
-                psph1->BHAccretor.iPid   = psph2->BHAccretor.iPid;
-                psph1->BHAccretor.iIndex = psph2->BHAccretor.iIndex;
-            }// Otherwise just keep the previous attempt
-        }
-
+        // If not, just keep the previous attempt
     }
 }
 
@@ -351,11 +347,16 @@ void initBHevolve(void *vpkd,void *vp) {
     if (pkdIsGas(pkd,p)) {
         SPHFIELDS *psph = pkdSph(pkd,p);
 
+#ifdef OLD_FB_SCHEME
         psph->Uint = 0.0;
         psph->E = 0.0;
 #ifdef ENTROPY_SWITCH
-        psph->S = 0.;
+        psph->S = 0.0;
 #endif
+#else // OLD_FB_SCHEME
+        psph->fAccFBEnergy = 0.0;
+#endif
+
         psph->BHAccretor.iPid = NOT_ACCRETED;
     }
 