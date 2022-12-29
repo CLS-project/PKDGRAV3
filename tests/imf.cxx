5f671428799988b7474c9eaaaeec7b4e50e7378f
Alex Massaro <alex.massaro@iac.es>
2022-12-29 19:06:42 -0400

    PKDGRAV-143 Added Kroupa and Salpeter IMFs to the unittest

--- tests/imf.cxx
+++ tests/imf.cxx
@@ -11,21 +11,42 @@ protected:
 };
 
 
-TEST_F(IMFTest, ChabrierIMFTest) {
+TEST_F(IMFTest, BasicIMFTest) {
+    const double dMinMass{0.1};
+    const double dMaxMass{100.0};
+
     // Try to load an unexisting IMF
     EXPECT_DEATH({
-        auto wrongIMF = ChooseIMF("other", 0.1, 100.0);
+        auto wrongIMF = ChooseIMF("other", dMinMass, dMaxMass);
     }, "Undefined IMF type has been given to ChooseIMF: other");
 
-    auto IMF = ChooseIMF("chabrier", 0.1, 100.0);
+    auto ChabrierIMF = ChooseIMF("chabrier", dMinMass, dMaxMass);
+    auto KroupaIMF = ChooseIMF("kroupa", dMinMass, dMaxMass);
+    auto SalpeterIMF = ChooseIMF("salpeter", dMinMass, dMaxMass);
 
-    // Check that the IMF is correctly normalized after initialization
-    double norm = IMF->MassWeightedIntegration(0.1, 100.0);
-    EXPECT_EQ(norm, 1.);
+    // Check that the IMFs are correctly normalized after initialization
+    double norm = ChabrierIMF->MassWeightedIntegration(dMinMass, dMaxMass);
+    EXPECT_NEAR(norm, 1.0, 1e-13);
+
+    norm = KroupaIMF->MassWeightedIntegration(dMinMass, dMaxMass);
+    EXPECT_NEAR(norm, 1.0, 1e-13);
+
+    norm = SalpeterIMF->MassWeightedIntegration(dMinMass, dMaxMass);
+    EXPECT_NEAR(norm, 1.0, 1e-13);
 
     // Compare with results from Dalla Vecchia & Schaye 2012, Section 2
-    double test1 = IMF->UnweightedIntegration(6.,100.0);
+    double test1 = ChabrierIMF->UnweightedIntegration(6.0, dMaxMass);
     EXPECT_NEAR(test1, 0.01736, 1e-5);
-    double test2 = IMF->UnweightedIntegration(8.,100.0);
+    double test2 = ChabrierIMF->UnweightedIntegration(8.0, dMaxMass);
     EXPECT_NEAR(test2, 0.01180, 1e-5);
+
+    test1 = KroupaIMF->UnweightedIntegration(6.0, dMaxMass);
+    EXPECT_NEAR(test1, 0.01736, 3e-3);
+    test2 = KroupaIMF->UnweightedIntegration(8.0, dMaxMass);
+    EXPECT_NEAR(test2, 0.01180, 3e-3);
+
+    test1 = SalpeterIMF->UnweightedIntegration(6.0, dMaxMass);
+    EXPECT_NEAR(test1, 0.01107, 1e-5);
+    test2 = SalpeterIMF->UnweightedIntegration(8.0, dMaxMass);
+    EXPECT_NEAR(test2, 0.00742, 1e-5);
 }
