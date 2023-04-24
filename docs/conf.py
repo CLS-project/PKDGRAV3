cd87128a72a98b3aa696161765269cf2f94b8fa2
Thomas Meier <meiers.thomas@bluewin.ch>
2023-04-24 15:17:36 +0200

    Sphinx configuration now automatic

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import sys,os
+import sys,os,sphinx
 
 sys.path.append(os.path.abspath("./_ext"))
 
@@ -17,19 +17,18 @@ author = 'Douglas Potter'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# If there is an error like WARNING: using "math" markup without a Sphinx math extension active
-# switch to the second line
-extensions = ['make_parameters']
-# extensions = ['make_parameters','sphinx.ext.imgmath']
+extensions=['make_parameters']
+if sphinx.version_info < (1,8):
+    extensions += ['sphinx.ext.imgmath']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# Uncomment if there is an error like ....contents.rst not found
-# master_doc = 'index'
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+if sphinx.version_info >= (4,0):
+    root_doc = 'index'
+else:
+    master_doc = 'index'
 html_theme = 'alabaster'
 html_static_path = ['_static']
