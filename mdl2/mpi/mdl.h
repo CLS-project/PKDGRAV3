7787eee9fca98adb35c8343e3975076ae9e98c37
Douglas Potter <douglas.potter@uzh.ch>
2023-06-07 13:54:36 +0200

    fix: MDL pack function always receive a valid data pointer. Previously they would receive a nullptr when elements did not exist and they were expected to take an appropriate action, but there is no sensible action other than the default which is to zero out (or even do nothing).

--- mdl2/mpi/mdl.h
+++ mdl2/mpi/mdl.h
@@ -103,7 +103,7 @@ class CACHEhelper {
 protected:
     uint32_t nData;   // Size of a data element in bytes
     bool     bModify; // If this cache will be modified (and thus flushed)
-    virtual void    pack(void *dst, const void *src) { if (src) memcpy(dst,src,nData); else memset(dst,0,nData); }
+    virtual void    pack(void *dst, const void *src) { memcpy(dst,src,nData); }
     virtual void  unpack(void *dst, const void *src, const void *key) { memcpy(dst,src,nData); }
     virtual void    init(void *dst) {}
     virtual void   flush(void *dst, const void *src)                  { memcpy(dst,src,nData); }
